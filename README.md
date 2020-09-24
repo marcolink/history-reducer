340f67ca8d43175e0cafc716ecc84f5855a01906
Marco Link <marco.link@contentful.com>
2020-09-24 16:05:46 +0200

    fix type name for dispatch, add more readme content

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # history-reducer
 Dead simple react hook to create state history based on react useReducer. 
 
+## install
+```
+yarn add history-reducer
+```
+(TS declarations are shipped within this package)
+
+## Usage
+
+### Actions
+| Action   | Description   |
+|----------|-------------|
+|`push`    |push new current state|
+|`back`    |make the prev state the current, and push the current to the future stack|
+|`forward` |if available, push the last future state to the current, and make the current the last past state|
+|`reset`   |reset past and future state, and optional set a new current state|
+
+### Hook
+
+```js
+const [{present, past, future}, dispatch] = useHistoryReducer(0, 5);
+```
+The hook `useHistoryReducer` returns the current state including `present`, `past` and `future` as the first argument, and a `dispatch` function as second. It accepts a default value as a first argument, and the max length for `past` and `future` as a optionao second argument. 
+
+
 ## Example
+#### Basic
 ```js
 import {useHistoryReducer} from "history-reducer";
 
@@ -11,9 +36,7 @@ const Example = () => {
         <div>
             <p>Current value is {present}, but has been {past} before</p>
             <button onClick={() => dispatch({type:'push', state: present + 1})}>add</button>
-            <button onClick={() => dispatch({type: Operation.BACK})}>back</button>
-            <button onClick={() => dispatch({type: Operation.FORWARD})}>forward</button>
-            <button onClick={() => dispatch({type: Operation.RESET, state: 0})}>reset</button>
+            <button onClick={() => dispatch({type: Action.BACK})}>back</button>
         </div>
     )
 }
--- test/reduceHistory.spec.ts
+++ test/reduceHistory.spec.ts
@@ -1,4 +1,4 @@
-import { createHistoryReducer, HistoryReduce, Operation } from '../src';
+import { createHistoryReducer, HistoryReduce, Action } from '../src';
 
 describe('A history reduce function', () => {
   let reduce: HistoryReduce;
@@ -10,42 +10,42 @@ describe('A history reduce function', () => {
   it('can push a new state', () => {
     const result = reduce(
       { present: null, past: [], future: [] },
-      { type: Operation.PUSH, state: 0 }
+      { type: Action.PUSH, state: 0 }
     );
     expect(result).toEqual({ present: 0, past: [], future: [] });
   });
   it('can push new state and generate past state', () => {
     const result = reduce(
       { present: 1, past: [3, 2], future: [] },
-      { type: Operation.PUSH, state: 0 }
+      { type: Action.PUSH, state: 0 }
     );
     expect(result).toEqual({ present: 0, past: [3, 2, 1], future: [] });
   });
   it('can walk history back', () => {
     const result = reduce(
       { present: 1, past: [3, 2], future: [] },
-      { type: Operation.BACK }
+      { type: Action.BACK }
     );
     expect(result).toEqual({ present: 2, past: [3], future: [1] });
   });
   it('can walk history back and stop at initial state', () => {
     const result = reduce(
       { present: 1, past: [], future: [2] },
-      { type: Operation.BACK }
+      { type: Action.BACK }
     );
     expect(result).toEqual({ present: 1, past: [], future: [2] });
   });
   it('can walk history forward', () => {
     const result = reduce(
       { present: 1, past: [], future: [3, 2] },
-      { type: Operation.FORWARD }
+      { type: Action.FORWARD }
     );
     expect(result).toEqual({ present: 2, past: [1], future: [3] });
   });
   it('can walk history forward and stop at initial state', () => {
     const result = reduce(
       { present: 1, past: [3], future: [] },
-      { type: Operation.FORWARD }
+      { type: Action.FORWARD }
     );
     expect(result).toEqual({ present: 1, past: [3], future: [] });
   });
